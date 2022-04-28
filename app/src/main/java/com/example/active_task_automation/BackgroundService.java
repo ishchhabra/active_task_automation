21fd22828fb482920c4ba0fd36895fe74d30ac91
Ish Chhabra <ishchhabra12@gmail.com>
2022-04-27 21:57:46 -0400

    .

--- app/src/main/java/com/example/active_task_automation/BackgroundService.java
+++ app/src/main/java/com/example/active_task_automation/BackgroundService.java
@@ -1,17 +1,21 @@
 package com.example.active_task_automation;
 
 import android.annotation.SuppressLint;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
+import android.os.Environment;
 import android.os.IBinder;
 import android.os.Looper;
+import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -23,11 +27,19 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BackgroundService extends Service {
     private final String NOTIFICATION_CHANNEL_ID = "location";
     private final String ACTION_STOP_SERVICE = "STOP_BACKGROUND_SERVICE";
+
+    private File gps_fd, dnd_fd, accelerometer_fd, barometer_fd;
+    private FileOutputStream gps_fos, dnd_fos, accelerometer_fos, barometer_fos;
 
     public BackgroundService() {
     }
@@ -65,11 +77,121 @@ public class BackgroundService extends Service {
             startForeground(1, builder.build());
         }
 
-        startRecordingLocationInfo();
+        startRecording();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        try {
+            gps_fos.close();
+            dnd_fos.close();
+            accelerometer_fos.close();
+            barometer_fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @SuppressLint("MissingPermission")
-    public void startRecordingLocationInfo() {
+    public void startRecording() {
+        // Open all files.
+        try {
+            File root_directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+            gps_fd = new File(root_directory, "gps_data.csv");
+            if (!gps_fd.exists()) {
+                gps_fd.createNewFile();
+            }
+            gps_fos = new FileOutputStream(gps_fd, true);
+
+            dnd_fd = new File(root_directory, "dnd_data.csv");
+            if (!dnd_fd.exists()) {
+                dnd_fd.createNewFile();
+            }
+            dnd_fos = new FileOutputStream(dnd_fd, true);
+
+            accelerometer_fd = new File(root_directory, "accelerometer_data.csv");
+            if (!accelerometer_fd.exists()) {
+                accelerometer_fd.createNewFile();
+            }
+            accelerometer_fos = new FileOutputStream(accelerometer_fd, true);
+
+            barometer_fd = new File(root_directory, "barometer_data.csv");
+            if (!barometer_fd.exists()) {
+                barometer_fd.createNewFile();
+            }
+            barometer_fos = new FileOutputStream(barometer_fd, true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        // Accelerometer information
+        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(new SensorEventListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                try {
+                    accelerometer_fos.write(
+                            String.format("%d;%f;%f;%f\n",
+                                    sensorEvent.timestamp,
+                                    sensorEvent.values[0],
+                                    sensorEvent.values[1],
+                                    sensorEvent.values[2]).getBytes()
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {}
+        }, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Pressure information
+        Sensor barometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        sensorManager.registerListener(new SensorEventListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                try {
+                    barometer_fos.write(
+                            String.format("%d;%f\n",
+                                    sensorEvent.timestamp,
+                                    sensorEvent.values[0]).getBytes()
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {}
+        }, barometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // DND information
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void run() {
+                try {
+                    dnd_fos.write(
+                            String.format("%d;%d\n",
+                                    System.currentTimeMillis(),
+                                    getDNDStatus()).getBytes()
+                    );
+                } catch (IOException | Settings.SettingNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 1000);
+
+        // GPS information
         FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         LocationRequest locationRequest = new LocationRequest();
@@ -83,16 +205,16 @@ public class BackgroundService extends Service {
                 Location location = locationResult.getLastLocation();
 
                 Log.d("GPS", "" + location.getLatitude());
-//                try {
-//                    gps_fos.write(
-//                            String.format("%d;%f;%f\n",
-//                                    location.getTime(),
-//                                    location.getLatitude(),
-//                                    location.getLongitude()).getBytes()
-//                    );
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
+                try {
+                    gps_fos.write(
+                            String.format("%d;%f;%f\n",
+                                    location.getTime(),
+                                    location.getLatitude(),
+                                    location.getLongitude()).getBytes()
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }, Looper.getMainLooper());
     }
@@ -101,5 +223,9 @@ public class BackgroundService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private int getDNDStatus() throws Settings.SettingNotFoundException {
+        return Settings.Global.getInt(getContentResolver(), "zen_mode");
     }
 }
--- app/src/main/java/com/example/active_task_automation/MainActivity.java
+++ app/src/main/java/com/example/active_task_automation/MainActivity.java
@@ -36,18 +36,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
-    private File gps_fd;
-    private FileOutputStream gps_fos;
-
-    private File dnd_fd;
-    private FileOutputStream dnd_fos;
-
-    private File accelerometer_fd;
-    private FileOutputStream accelerometer_fos;
-
-    private File barometer_fd;
-    private FileOutputStream barometer_fos;
-
     private final int BACKGROUND_LOCATION_PERMISSION_CODE = 43;
     private final int LOCATION_PERMISSION_CODE = 44;
     private final int WRITE_EXTERNAL_STORAGE_PERMISSION_CODE = 112;
@@ -94,21 +82,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        try {
-            gps_fos.close();
-            dnd_fos.close();
-            accelerometer_fos.close();
-            barometer_fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -139,48 +112,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
     @SuppressLint("MissingPermission")
     private void startRecording() {
-        // Open all files.
-        try {
-            File root_directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-
-            gps_fd = new File(root_directory, "gps_data.csv");
-            if (!gps_fd.exists()) {
-                gps_fd.createNewFile();
-            }
-            gps_fos = new FileOutputStream(gps_fd, true);
-
-            dnd_fd = new File(root_directory, "dnd_data.csv");
-            if (!dnd_fd.exists()) {
-                dnd_fd.createNewFile();
-            }
-            dnd_fos = new FileOutputStream(dnd_fd, true);
-
-            accelerometer_fd = new File(root_directory, "accelerometer_data.csv");
-            if (!accelerometer_fd.exists()) {
-                accelerometer_fd.createNewFile();
-            }
-            accelerometer_fos = new FileOutputStream(accelerometer_fd, true);
-
-            barometer_fd = new File(root_directory, "barometer_data.csv");
-            if (!barometer_fd.exists()) {
-                barometer_fd.createNewFile();
-            }
-            barometer_fos = new FileOutputStream(barometer_fd, true);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-        // Accelerometer information
-        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(new MyAccelerometerListener(), accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        // Pressure information
-        Sensor barometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-        sensorManager.registerListener(new MyBarometerListener(), barometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
         // GPS information
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
@@ -189,23 +120,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         }
 
         waitForLocationPermission(locationManager);
-
-        // DND information
-        new Timer().scheduleAtFixedRate(new TimerTask() {
-            @SuppressLint("DefaultLocale")
-            @Override
-            public void run() {
-                try {
-                    dnd_fos.write(
-                            String.format("%d;%d\n",
-                                    System.currentTimeMillis(),
-                                    getDNDStatus()).getBytes()
-                    );
-                } catch (IOException | Settings.SettingNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 0, 1000);
     }
 
     private void waitForLocationPermission(LocationManager locationManager) {
@@ -227,78 +141,10 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             Context applicationContext = getApplicationContext();
             applicationContext.startForegroundService(new Intent(applicationContext, BackgroundService.class));
+            finish();
         }
     }
 
-    private int getDNDStatus() throws Settings.SettingNotFoundException {
-        return Settings.Global.getInt(getContentResolver(), "zen_mode");
-    }
 
-//    private class MyLocationListener implements LocationListener {
-//        @SuppressLint("DefaultLocale")
-//        @Override
-//        public void onLocationChanged(@NonNull Location location) {
-//            try {
-//                Log.d("GPS", String.format("%d;%f;%f\n",
-//                        location.getTime(),
-//                        location.getLatitude(),
-//                        location.getLongitude()));
-//
-//                latitude.setText("" + location.getLatitude());
-//
-//                gps_fos.write(
-//                        String.format("%d;%f;%f\n",
-//                                location.getTime(),
-//                                location.getLatitude(),
-//                                location.getLongitude()).getBytes()
-//                );
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 
-    private class MyAccelerometerListener implements SensorEventListener {
-        @SuppressLint("DefaultLocale")
-        @Override
-        public void onSensorChanged(SensorEvent sensorEvent) {
-            try {
-                accelerometer_fos.write(
-                        String.format("%d;%f;%f;%f\n",
-                                sensorEvent.timestamp,
-                                sensorEvent.values[0],
-                                sensorEvent.values[1],
-                                sensorEvent.values[2]).getBytes()
-                );
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int i) {
-
-        }
-    }
-
-    private class MyBarometerListener implements SensorEventListener {
-        @SuppressLint("DefaultLocale")
-        @Override
-        public void onSensorChanged(SensorEvent sensorEvent) {
-            try {
-                barometer_fos.write(
-                        String.format("%d;%f\n",
-                                sensorEvent.timestamp,
-                                sensorEvent.values[0]).getBytes()
-                );
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int i) {
-
-        }
-    }
 }

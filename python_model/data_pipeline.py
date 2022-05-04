15590c4e8bffa909b30531c02d1be7e50060724d
Ish Chhabra <ishchhabra12@gmail.com>
2022-05-04 13:13:26 -0400

    .

--- python_model/data_pipeline.py
+++ python_model/data_pipeline.py
@@ -1,11 +1,13 @@
-from time import time
+from typing import Tuple
 import pandas
 import numpy as np
+
 
 DATA_FOLDER_PATH = "data"
 GPS_FILE_NAME = "gps_data.csv"
 DND_FILE_NAME = "dnd_data.csv"
-GPS_WINDOW_SIZE = 5000 # in milliseconds
+GPS_WINDOW_SIZE = 5000  # in milliseconds
+
 
 class DataPipeline:
     def __init__(self: "DataPipeline", participant: str):
@@ -16,31 +18,38 @@ class DataPipeline:
 
     @staticmethod
     def load_dnd(participant: str):
-        dataframe = pandas.read_csv(f"{DATA_FOLDER_PATH}/{participant}/{DND_FILE_NAME}", sep=";")
+        dataframe = pandas.read_csv(
+            f"{DATA_FOLDER_PATH}/{participant}/{DND_FILE_NAME}", sep=";"
+        )
         dataframe.columns = ["timestamp", "dnd_value"]
         return dataframe
 
     @staticmethod
     def load_gps(participant: str):
-        dataframe = pandas.read_csv(f"{DATA_FOLDER_PATH}/{participant}/{GPS_FILE_NAME}", sep=";")
+        dataframe = pandas.read_csv(
+            f"{DATA_FOLDER_PATH}/{participant}/{GPS_FILE_NAME}", sep=";"
+        )
         dataframe.columns = ["timestamp", "latitude", "longitude"]
         return dataframe
-    
+
     @staticmethod
     def time_synchronize(dnd_data: pandas.DataFrame, gps_data: pandas.DataFrame):
-        # Given two dataframes, take a window of length 2 * GPS_WINDOW_SIZE from [dnd_timestamp - GPS_WINDOW_SIZE, dnd_timestamp + GPS_WINDOW_SIZE]. 
+        # Given two dataframes, take a window of length 2 * GPS_WINDOW_SIZE from [dnd_timestamp - GPS_WINDOW_SIZE, dnd_timestamp + GPS_WINDOW_SIZE].
         # Find all the gps rows with timestamp in this range, and then average over axis=0.
         # Do this for all the rows in DND dataframe, and return new dataframe which has ['timestamp' (from dnd), 'dnd_value', 'latitude', 'longitude']
 
-        def dnd_windowed_average(timestamp: int): 
-            averages = []
-            start = timestamp[0] + GPS_WINDOW_SIZE
-            for i in range(start ,len(timestamp)-GPS_WINDOW_SIZE,2*GPS_WINDOW_SIZE):
-                window = timestamp[i-GPS_WINDOW_SIZE:i+GPS_WINDOW_SIZE]
-                averages.append(np.average(window, axis=0))
-            return averages
+        # dnd_data_np = dnd_data.to_numpy()
+        # gps_data_np = dnd_data.to_numpy()
+
+        def gps_windowed_average(timestamp: int) -> Tuple[int, int]:
+            rows = gps_data.loc[
+                (gps_data["timestamp"] >= timestamp - GPS_WINDOW_SIZE)
+                & (gps_data["timestamp"] <= timestamp + GPS_WINDOW_SIZE)
+            ]
+
+            return rows.mean(axis=0)
 
         for i in range(dnd_data.size):
-            dnd_windowed_average(dnd_data["timestamp"].to_numpy(int))
+            gps_windowed_average(dnd_data["timestamp"][i])
 
         return dnd_data.merge(gps_data, on="timestamp")
--- python_model/main.py
+++ python_model/main.py
@@ -2,4 +2,4 @@ from data_pipeline import DataPipeline
 
 if __name__ == "__main__":
     rupak_data = DataPipeline("rupak")
-    # breakpoint()
+    breakpoint()

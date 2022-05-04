6b6615bcbadb697963b8687b3b9b045cb4f8447c
Ish Chhabra <ishchhabra12@gmail.com>
2022-05-04 19:57:39 -0400

    .

--- python_model/data_pipeline.py
+++ python_model/data_pipeline.py
@@ -1,24 +1,51 @@
+from tkinter import dnd
 from typing import Tuple
-import pandas
+import pandas as pd
 import numpy as np
+import os
 
 
 DATA_FOLDER_PATH = "data"
 GPS_FILE_NAME = "gps_data.csv"
 DND_FILE_NAME = "dnd_data.csv"
+BAROMETER_FILE_NAME = "barometer_data.csv"
+ACCELEROMETER_FILE_NAME = "accelerometer_data.csv"
 GPS_WINDOW_SIZE = 5000  # in milliseconds
 
 
 class DataPipeline:
-    def __init__(self: "DataPipeline", participant: str):
-        dnd_data = self.load_dnd(participant)
-        gps_data = self.load_gps(participant)
+    def __init__(
+        self: "DataPipeline",
+        participant: str,
+        use_barometer: bool = False,
+        use_accelerometer: bool = False,
+    ):
+        if os.path.exists(f"{DATA_FOLDER_PATH}/{participant}/cached_data.csv"):
+            self.dataframe = pd.read_csv(
+                f"{DATA_FOLDER_PATH}/{participant}/cached_data.csv", sep=";"
+            )
+        else:
+            dnd_data = self.load_dnd(participant)
+            gps_data = self.load_gps(participant)
 
-        self.dataframe = self.time_synchronize(dnd_data, gps_data)
+            barometer_data = None
+            if use_barometer is True:
+                barometer_data = self.load_barometer(participant)
+
+            if use_accelerometer is True:
+                accelerometer_data = self.load_accelerometer(participant)
+
+            self.dataframe = self.time_synchronize(
+                dnd_data, gps_data, barometer_data, accelerometer_data
+            )
+
+            self.dataframe.to_csv(
+                f"{DATA_FOLDER_PATH}/{participant}/cached_data.csv", sep=";"
+            )
 
     @staticmethod
     def load_dnd(participant: str):
-        dataframe = pandas.read_csv(
+        dataframe = pd.read_csv(
             f"{DATA_FOLDER_PATH}/{participant}/{DND_FILE_NAME}", sep=";"
         )
         dataframe.columns = ["timestamp", "dnd_value"]
@@ -26,16 +53,86 @@ class DataPipeline:
 
     @staticmethod
     def load_gps(participant: str):
-        dataframe = pandas.read_csv(
+        dataframe = pd.read_csv(
             f"{DATA_FOLDER_PATH}/{participant}/{GPS_FILE_NAME}", sep=";"
         )
         dataframe.columns = ["timestamp", "latitude", "longitude"]
         return dataframe
 
     @staticmethod
-    def time_synchronize(dnd_data: pandas.DataFrame, gps_data: pandas.DataFrame):
-        def gps_windowed_average(row) -> pandas.Series:
-            timestamp = row['timestamp']
+    def load_barometer(participant: str):
+        dataframe = pd.read_csv(
+            f"{DATA_FOLDER_PATH}/{participant}/{BAROMETER_FILE_NAME}", sep=";"
+        )
+        dataframe.columns = ["timestamp", "barometer"]
+        dataframe["timestamp"] = dataframe["timestamp"]
+        return dataframe
+
+    @staticmethod
+    def load_accelerometer(participant: str):
+        dataframe = pd.read_csv(
+            f"{DATA_FOLDER_PATH}/{participant}/{ACCELEROMETER_FILE_NAME}", sep=";"
+        )
+        dataframe.columns = [
+            "timestamp",
+            "accelerometer_x",
+            "accelerometer_y",
+            "accelerometer_z",
+        ]
+        dataframe["timestamp"] = dataframe["timestamp"]
+        return dataframe
+
+    @staticmethod
+    def time_synchronize(
+        dnd_data: pd.DataFrame,
+        gps_data: pd.DataFrame,
+        barometer_data: pd.DataFrame = None,
+        accelerometer_data: pd.DataFrame = None,
+    ):
+        # Minor hack on sensors' timestamps since it wasn't recorded correctly
+        if barometer_data is not None:
+            barometer_data["timestamp"] = barometer_data["timestamp"] / 1000
+            barometer_data["timestamp"] = barometer_data["timestamp"] - (
+                barometer_data["timestamp"][0] - dnd_data["timestamp"][0]
+            )
+
+        if accelerometer_data is not None:
+            accelerometer_data["timestamp"] = accelerometer_data["timestamp"] / 1000
+            accelerometer_data["timestamp"] = accelerometer_data["timestamp"] - (
+                accelerometer_data["timestamp"][0] - dnd_data["timestamp"]
+            )
+
+        def accelerometer_windowed_averge(row) -> pd.DataFrame:
+            timestamp = row["timestamp"]
+
+            rows = accelerometer_data.loc[
+                (accelerometer_data["timestamp"] >= timestamp - GPS_WINDOW_SIZE)
+                & (accelerometer_data["timestamp"] <= timestamp + GPS_WINDOW_SIZE)
+            ]
+
+            avg = rows.mean(axis=0)
+
+            return np.sqrt(
+                (
+                    np.square((avg["accelerometer_x"]))
+                    + np.square((avg["accelerometer_y"]))
+                    + np.square((avg["accelerometer_z"]))
+                )
+            )
+
+        def barometer_windowed_average(row) -> pd.DataFrame:
+            timestamp = row["timestamp"]
+
+            rows = barometer_data.loc[
+                (barometer_data["timestamp"] >= timestamp - GPS_WINDOW_SIZE)
+                & (barometer_data["timestamp"] <= timestamp + GPS_WINDOW_SIZE)
+            ]
+
+            avg = rows.mean(axis=0)
+            return avg["barometer"]
+
+        def gps_windowed_average(row) -> pd.Series:
+            timestamp = row["timestamp"]
 
             rows = gps_data.loc[
                 (gps_data["timestamp"] >= timestamp - GPS_WINDOW_SIZE)
@@ -43,8 +140,21 @@ class DataPipeline:
             ]
 
             avg = rows.mean(axis=0)
-            return pandas.Series([avg['latitude'], avg['longitude']])
+            return pd.Series([avg["latitude"], avg["longitude"]])
 
         result_df = dnd_data.copy()
-        result_df[['latitude', 'longitude']] = result_df.apply(gps_windowed_average, axis=1)
+        result_df[["latitude", "longitude"]] = result_df.apply(
+            gps_windowed_average, axis=1
+        )
+
+        if barometer_data is not None:
+            result_df["barometer_mean"] = result_df.apply(
+                barometer_windowed_average, axis=1
+            )
+
+        if accelerometer_data is not None:
+            result_df["accelerometer_mean"] = result_df.apply(
+                accelerometer_windowed_averge, axis=1
+            )
+
         return result_df
---
+++ python_model/knn.py
@@ -0,0 +1,28 @@
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import statistics
+
+
+def knn(dataframes: list):
+    cv_scores = []
+    for i in range(len(dataframes)):
+        train_dataframes = dataframes[:i] + dataframes[i + 1 :]
+
+        model = KNeighborsClassifier(n_neighbors=5)
+
+        train_df = pd.concat(train_dataframes)
+        train_df.dropna(inplace=True)
+        X_train = train_df.drop(columns=["dnd_value"])
+        y_train = train_df["dnd_value"]
+        model.fit(X_train, y_train)
+
+        test_df: pd.DataFrame = dataframes[i]
+        test_df.dropna(inplace=True)
+        X_test = test_df.drop(columns=["dnd_value"])
+        y_test = test_df["dnd_value"]
+
+        cv_scores.append(model.score(X_test, y_test))
+
+    print(
+        f"KNN accuracy with leave-one-participant-out is {statistics.mean(cv_scores)}"
+    )
